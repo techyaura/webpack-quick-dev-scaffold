5a594a25cad745e46a880b49e598737b95a10ae8
techyaura <techyaura@gmail.com>
2021-07-13 14:01:05 +0530

    feat(webpack): webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    liveReload: true,
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
