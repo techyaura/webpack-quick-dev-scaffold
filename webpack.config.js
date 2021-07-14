74826deea04085aa68d495891ae2b155afa36662
techyaura <techyaura@gmail.com>
2021-07-14 18:21:34 +0530

    add webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,38 @@ module.exports = {
     open: true,
     liveReload: true,
   },
-  plugins: [new HtmlWebpackPlugin()]
+  module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: ["babel-loader"]
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.css?$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader'
+            },
+          ],
+        }
+      ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: true,
+  })]
 };
